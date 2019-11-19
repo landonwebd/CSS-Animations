4ea2215cba23214d1cb09875a01745a832418d42
Landon Baker <Landon@kicksite.net>
2019-11-19 11:19:47 -0600

    Added comments to script.js file

--- script.js
+++ script.js
@@ -1,40 +1,24 @@
-// window.onscroll = function() {
-//   myFunction();
-// };
 
-// function myFunction() {
-//   /**
-//    * The scrollTop() method sets or returns the vertical scrollbar position 
-//    * for the selected elements.
-//    * 
-//    * The body method returns the <body> element. the documentElement returns the <html> element
-//    */
-
-//   if (document.body.scrollTop > 1500 || document.documentElement.scrollTop > 1500 ) {
-//     document.querySelector(".card-image, .card-text").className += " slide-in";
-//   }
-// // }
-
-
-
-// window.onscroll = function() {
-//   this.animation();
-// }
-
-// function animation() {
-//   if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000 ) {
-//     document.querySelector('.card').className = ' slide-in';
-//   }
-// }
-
+/**
+ * @param {function} func - The function to add delay to
+ * @param {number} wait - The time in milliseconds between function firing.
+ * @param {boolean} immediate - If this function should run on page load
+ */
 function debounce( func, wait = 5, immediate = true ) {
   let timeout;
   return function () {
     let context = this
+
+    /**
+     * The arguments object is an array-like object. It has a length property that corresponds to 
+     * the number of arguments passed into the function. You can access these values by indexing 
+     * into the array, e.g. arguments[0] is the first argument. 
+     */
+
     let args = arguments;
     let later = function () {
       timeout = null;
-      if( !immediate ) func.apply( context, args );
+      if( !immediate ) func.apply( context, args ); // https://www.w3schools.com/js/js_function_apply.asp
     }
     let callNow = immediate && !timeout;
     clearTimeout( timeout );
@@ -43,20 +27,47 @@ function debounce( func, wait = 5, immediate = true ) {
   };
 }
 
+/**
+ * grabs all of the elements you want to slide in and puts them into an array to cycle through
+ */
 const sliderImages = document.querySelectorAll( `.slide-in` );
 
 function checkSlide( e ) {
+  /**
+   * The .scrollY method returns how many pixels you have scrolled down.
+   * the .innerHeight method returns the height of the viewport
+   */
   console.log( `You have scrolled down ${ window.scrollY } pixels` );
   console.log( `The bottom of the viewport is currently ${ window.scrollY + window.innerHeight } pixels from the top of the window` );
+
+  /**
+   * using a forEach to cycle through each of the elements you want to have the slide-in effect
+   */
   sliderImages.forEach( slideImage => {
+    /**
+     * The offsetTop property returns the top position (in pixels) relative to the top of the 
+     * offsetParent element.
+     */
     console.log( `The offsetTop of the image is ${ slideImage.offsetTop } pixels` );
     console.log( `The height of the image is ${ slideImage.height } pixels` )
-    // half way through the image
+    /**
+     * We want the slide in to happen when the user has scrolled past half of the sliding element.
+     * To calculate that, we get the current pixel count of the bottom the viewport. We then 
+     * subtract half of the height of the image to get the pixel count we want the slide in to 
+     * happen.
+     */
     const slideInAt = ( window.scrollY + window.innerHeight ) - slideImage.height * 0.5;
-    // bottom of the image
-    const imageBottom = slideImage.offsetTop + slideImage.height;
 
+    /**
+     * A Boolean to once the slideInAt pixel count is finally bigger than the top position of
+     * the element.
+     */
     const isHalfShown = slideInAt > slideImage.offsetTop;
+
+    /**
+     * Once the isHalfShown is true, the function below triggers, adding the .active class to
+     * the element, thus triggering the animation
+     */
 
     if( isHalfShown ) {
       slideImage.classList.add( 'active' );
@@ -65,4 +76,8 @@ function checkSlide( e ) {
 
 }
 
+/**
+ * adding the event listener 'scroll' to the window object and telling it to run the debounce
+ * function on each scroll.
+ */
 window.addEventListener( `scroll`, debounce( checkSlide ) );
