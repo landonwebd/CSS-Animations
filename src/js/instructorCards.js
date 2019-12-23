2de15289e7d4927e64ff3d14f418d48b4209df6b
Landon Baker <Landon@kicksite.net>
2019-12-23 15:33:36 -0600

    Instructor Cards Stuff
    Adding Instructor Stuff

--- src/js/instructorCards.js
+++ src/js/instructorCards.js
@@ -1,9 +1,82 @@
 const instructorCards = document.querySelectorAll(`.instructor`);
+const instructorBios = document.querySelectorAll(`.instructor-bio > p`);
+let showChar = 150;
+let ellipsestext = `...`;
+
+instructorBios.forEach(biography => {
+  let content = biography.innerHTML;
+  if(content.length > showChar) {
+    let contentToShow = content.substr(0, showChar);
+    let contentToHide = content.substr(showChar - 1, content.length - showChar);
+    let html = contentToShow + `<span class="bio-ellipses">${ ellipsestext }</span>
+      <span class="hidden-text">${ contentToHide }</span>`;
+    biography.innerHTML = html;
+  }
+});
 
 function enlargeCard() {
-  this.style.background = `green`;
+  this.removeEventListener(`click`, enlargeCard);
+  let cardNumber = this.getAttribute(`data-number`);
+  let bioEllipses = this.querySelector(`.bio-ellipses`);
+  let hiddenText = this.querySelector(`span.hidden-text`);
+  let readMore = this.querySelector(`.read-more`);
+  bioEllipses.innerHTML = ``;
+  readMore.innerHTML = `Show Less`;
+  readMore.classList.add(`show-less-active`);
+  hiddenText.style.display = `block`;
+  this.classList.add(`instructor-active`);
+  this.classList.remove(`return-instructor`);
+
+  if(cardNumber % 3 === 2) {
+    this.classList.add(`middle-card-active`);
+  } else if(cardNumber % 3 === 1) {
+    this.classList.add(`left-card-active`);
+  } else if(cardNumber % 3 === 0) {
+    this.classList.add(`right-card-active`);
+  }
+
+  let activeCard = this;
+
+  document.addEventListener(`click`, checkClick);
+
+  function checkClick() {
+    let isClickInside = activeCard.contains(event.target);
+    if(!isClickInside) {
+      returnShit();
+      activeCard.addEventListener(`click`, enlargeCard);
+      document.removeEventListener(`click`, checkClick);
+      return;
+    }
+    let checkClick = activeCard.querySelector(`.read-more`);
+    let isButtonClicked = checkClick.contains(event.target);
+    if(isButtonClicked) {
+      returnShit();
+      activeCard.addEventListener(`click`, enlargeCard);
+      document.removeEventListener(`click`, checkClick);
+      return;
+    }
+  }
+
+  function returnShit() {
+    activeCard.classList.remove(`instructor-active`);
+    if(cardNumber % 3 === 2) {
+      activeCard.classList.remove(`middle-card-active`);
+    } else if(cardNumber % 3 === 1) {
+      activeCard.classList.remove(`left-card-active`);
+    } else if(cardNumber % 3 === 0) {
+      activeCard.classList.remove(`right-card-active`);
+    }
+    activeCard.classList.add(`return-instructor`);
+    activeCard.parentNode.style.removeProperty(`justify-self`);
+    bioEllipses.innerHTML = ellipsestext;
+    readMore.innerHTML = `Read More`;
+    hiddenText.style.display = `none`;
+    readMore.classList.toggle(`show-less-active`);
+  }
 }
 
 for(let i = 0; i < instructorCards.length; i++) {
   instructorCards[ i ].addEventListener(`click`, enlargeCard);
+  instructorCards[ i ].setAttribute(`data-number`, i + 1);
 }
+
